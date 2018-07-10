d681c35cb9db9f98958bff9216b827c14bf0874b
Prateek Chaplot <prateek.chaplot@metacube.com>
2018-07-10 18:46:16 +0530

    Basic functioning of the application done

--- bindingValidSample/MainWindow.xaml.cs
+++ bindingValidSample/MainWindow.xaml.cs
@@ -20,9 +20,26 @@ namespace bindingValidSample
     /// </summary>
     public partial class MainWindow : Window
     {
+        string objName;
+        string objEmail;
+        string objPhone;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Fetching data from Entry Details
+            objName = name.Text;
+            objEmail = email.Text;
+            objPhone = phone.Text;
+
+            // Providing data to View Details
+            displayname.Text = objName;
+            displayemail.Text = objEmail;
+            displayphone.Text = objPhone;
         }
     }
 }

3a8c74fb219191defc5107be58acac6da182eb70
Prateek Chaplot <prateek.chaplot@metacube.com>
2018-07-16 18:51:21 +0530

    TODO: remaining...

--- bindingValidSample/MainWindow.xaml.cs
+++ bindingValidSample/MainWindow.xaml.cs
@@ -1,27 +1,36 @@
-﻿// make local variable to be binded not other way around
-// remove trigger in event and declare in style triggers : make submit button disabled@start and enabled iff no error in textbox
+﻿// remove trigger in event and declare in style triggers : make submit button disabled@start and enabled iff no error in textbox
 // use inbuilt validations for rangerules
+// get familiar with culture info
 // exception handling
 
 using System.Windows;
+using Microsoft.Practices.EnterpriseLibrary.Validation;
 
 namespace bindingValidSample
 {
     public partial class MainWindow : Window
     {
+        private MyDataSource _dataSource = new MyDataSource();
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = _dataSource;
         }
 
         private void Submit_Click(object sender, RoutedEventArgs e)
         {
-            MyDataSource mds = (MyDataSource)this.TryFindResource("Ods");
-            
+            // validation testing
+            Validator validator = ValidationFactory.CreateValidator<MyDataSource>();
+            ValidationResults results = new ValidationResults();
+            validator.Validate(_dataSource, results);
+
+            MessageBox.Show("Is _datasource valid? " + results.IsValid.ToString());
+
             // Providing data to View Details
-            displayname.Text = mds.name;
-            displayemail.Text = mds.email;
-            displayphone.Text = mds.phone;
+            displayname.Text = _dataSource.name;
+            displayemail.Text = _dataSource.email;
+            displayphone.Text = _dataSource.phone;
         }
     }
 }
--- bindingValidSample/MyDataSource.cs
+++ bindingValidSample/MyDataSource.cs
@@ -1,16 +1,22 @@
-﻿namespace bindingValidSample
+﻿using System;
+using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
+
+namespace bindingValidSample
 {
     public class MyDataSource
     {
+        [NotNullValidator]
         public string name { get; set; }
+
+        [NotNullValidator]
         public string email { get; set; }
+
+        [NotNullValidator]
         public string phone { get; set; }
 
         public MyDataSource()
         {
-            name = null;
-            email = null;
-            phone = null;
         }
     }
 }
